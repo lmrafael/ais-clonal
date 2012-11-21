e177dbd5150d22d5feea9bebc87c88c2c06c6456
lmrafael <rafaumm@gmail.com>
2012-11-21 15:52:17 -0300

    advancing

--- src/ais/Main.java
+++ src/ais/Main.java
@@ -1,22 +1,24 @@
 package ais;
 
+import java.util.ArrayList;
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-//		train(training set)
-//		wait until (an e-mail arrives or a user action is intercepted)
-//		ag - convert e-mail into antigen
-//		IF(ag requires classification)
-//		classify(ag)
-//		IF(ag classified as uninteresting)
-//		move ag into user accessible storage
-//		ELSE
-//		allow e-mail to pass through
-//		IF(user has given feedback on ag)
-//		update_population(ag)
+		
+		ArrayList<BankStatement> bankStatements = new ArrayList<BankStatement>();
+		bankStatements.add(File.read("Bank Statement.txt"));
+		Train.start(bankStatements);
+		//	wait until (an e-mail arrives or a user action is intercepted)
+		//	ag - convert e-mail into antigen
+		//	IF(ag requires classification)
+		//	classify(ag)
+		//	IF(ag classified as uninteresting)
+		//	move ag into user accessible storage
+		//	ELSE
+		//	allow e-mail to pass through
+		//	IF(user has given feedback on ag)
+		//	update_population(ag)
 
 	}
 
--- src/ais/Train.java
+++ src/ais/Train.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 
 public class Train {
 
-	public void start(ArrayList<BankStatement> bankStatements){
+	public static void start(ArrayList<BankStatement> bankStatements){
 		for (BankStatement bankStatement : bankStatements) {
-			BCell = new BCell()
+			for (Expense expense : bankStatement.getExpenses()) {
+				BCell bCell = new BCell();
+				bCell.setValue(Double.parseDouble(expense.getValue()));
+				String[] descriptionSplit = expense.getDescription().split(";");
+				ArrayList<String> descriptionList = new ArrayList<String>();
+				for (int i = 0; i < descriptionSplit.length; i++) {
+					descriptionList.add(descriptionSplit[i]);
+				}
+				bCell.setDescription(descriptionList);
+				bCell.setCountry(expense.getCountry());
+				//add subject words and sender words to appropriate library
+			}
+
+		
 		}
 	}
 	
-//	process e-mail into a B-cell
-//	add subject words and sender words
-//	to appropriate library
 //	remove Kt random elements from TE
 //	and insert into MC
 //	FOREACH(mc  MC)
